b955896188b2ac4a18ed84103df76a254e39d455
root <hnewberry0304@gmail.com>
2022-09-18 14:01:44 -0700

    hi

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
-#include <stdio.h>
-
-int main(void)
 {
-  char str[] = "programinng is like building a multilingual puzzle";
-  puts(str);
-  return 0;
+int main(void);
+char str[] = "Programming is like building a mulilingual puzzle";
+
+puts(str);
+return 0;
 }
