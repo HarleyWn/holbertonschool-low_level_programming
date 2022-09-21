60e85213d2438799527f4e497bc859d5b88dd41b
root <hnewberry0304@gmail.com>
2022-09-21 13:47:13 -0700

    s

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -8,14 +8,4 @@
 int add(int one, int two)
 {
   return (one + two);
-}#include "main.h"
-/**
-* add - a function
-* @one: first
-* @two: second
-* Return: one + two
-*/
-int add(int one, int two)
-{
-return (one + two);
 }
